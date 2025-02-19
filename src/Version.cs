159ffe3a3ac160133b64bc7a34f6ad349e27b115
Kalle <info@kalleminkner.de>
2025-02-19 17:21:13 +0100

    bump version to 0.0.2

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace PlayerSessions
 {
     public partial class PlayerSessions : BasePlugin
     {
-        public override string ModuleVersion => "0.0.1";
+        public override string ModuleVersion => "0.0.2";
     }
 }
