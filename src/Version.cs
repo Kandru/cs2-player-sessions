8117f43dab7c11b4468e7e147321bcade492b1e1
Kalle <kalle@kandru.de>
2025-02-23 18:37:44 +0100

    bump version to 0.0.21

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace PlayerSessions
 {
     public partial class PlayerSessions : BasePlugin
     {
-        public override string ModuleVersion => "0.0.20";
+        public override string ModuleVersion => "0.0.21";
     }
 }
